32f6d298488fc21cb4917ab0646492ccc8add544
Andluri Vivekananda Reddy <120734128+VivekanandaReddy2233@users.noreply.github.com>
2025-11-28 04:05:55 +0530

    Update tutorial.service.ts

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://20.41.115.205/api/tutorials';
+const baseUrl = 'http://20.41.116.110/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
